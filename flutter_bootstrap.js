eec1ba905f0e1e3f974f6751f8c2f5182ce3b0e9
nuhao0 <nuhanuna2004@gmail.com>
2026-09-25 22:17:16 +0300

    Deploy to gh-pages directly from local

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1697115406"
+    serviceWorkerVersion: "4009042727"
   }
 });
